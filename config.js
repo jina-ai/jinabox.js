dc06a563c983e24e921f9886dddf237e51eb89af
Sergiy Chemodanov <sergiy@oakah.com>
2020-06-10 18:23:37 -0500

    removed unnecessary files, update package.json

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	jinaURL: 'localhost:45678'
-}
--- src/components/SearchResult.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SearchResult({ content, type, search }) {
-	return (
-		<div className="search-result" onClick={() => search && search([content])}>
-			{content}
-		</div>
-	)
-}
