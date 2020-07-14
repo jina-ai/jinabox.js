486b3b1f98135c5e196470f7a10953bcb352662e
XiaoQu <qulianqing1314@qq.com>
2020-07-14 16:08:04 +0800

    chore(rollup): split config file with prod and local, also split command.

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,14 +1,24 @@
 const nodeResolve = require('@rollup/plugin-node-resolve').default;
 const babel = require('rollup-plugin-babel');
+const { terser } = require('rollup-plugin-terser');
 const postcss = require('rollup-plugin-postcss');
 
 const babelConfig = require('./babel.config');
 const extensions = ['.js'];
 
+const terserConfig = {
+  compress: {
+    pure_getters: true,
+    unsafe: true,
+    unsafe_comps: true,
+    warnings: false,
+  }
+};
+
 module.exports = {
     input: 'src/jinabox.js',
     output: {
-      file: 'dist/jinabox.umd.js',
+      file: 'dist/jinabox.umd.min.js',
       format: 'umd',
     },
     plugins: [
@@ -18,6 +28,7 @@ module.exports = {
         extensions,
         exclude: 'node_modules/**',
       }),
+      terser(terserConfig),
       postcss(),
     ],
   };
