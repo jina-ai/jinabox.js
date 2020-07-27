aead40685bb4a69052ee38db5aed293efae268a3
Sergiy Chemodanov <sergiy@oakah.com>
2020-07-27 09:37:58 -0500

    feat: configure floater settings

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ const settings = {
 	showSearchbarDropzone: true,
 	showFloaterDropzone: true,
 	theme: 'default',
-	placeholders:['type or drag anything to search', 'powered by Jina', 'unleash your curiosity and happy searching'],
+	placeholders: ['type or drag anything to search', 'powered by Jina', 'unleash your curiosity and happy searching'],
 	typewriterDelayCharacter: 50,
 	typewriterDelayItem: 1000,
+	floaterStyle: 'standard'
 };
 
 const defaultComponentSettings = {
@@ -32,7 +33,7 @@ const defaultComponentSettings = {
 	searchIcon: 'color',
 	floaterIcon: 'color',
 	globalDrag: true,
-	placeholders:['type or drag anything to search', 'powered by Jina', 'unleash your curiosity and happy searching'],
+	placeholders: ['type or drag anything to search', 'powered by Jina', 'unleash your curiosity and happy searching'],
 }
 
 const searchbarSettings = {}
@@ -47,90 +48,93 @@ const placeholderKeystrokeDelayInput = document.getElementById('placeholderKeyst
 renderPlaceholders();
 placeholderDelayInput.value = settings.typewriterDelayItem;
 placeholderKeystrokeDelayInput.value = settings.typewriterDelayCharacter;
-placeholderInput.addEventListener('change',function(e){
+placeholderInput.addEventListener('change', function (e) {
 	const text = e.target.value;
 	addPlaceholder(text);
 	placeholderInput.value = ''
 });
-placeholderDelayInput.addEventListener('change',function(e){
+placeholderDelayInput.addEventListener('change', function (e) {
 	const delay = e.target.value;
 	settings.typewriterDelayItem = delay;
-	document.getElementById('main-searchbar').setAttribute('typewriterDelayItem', delay);
-	document.getElementById('main-floater').setAttribute('typewriterDelayItem', delay);
 	renderCode();
 })
-placeholderKeystrokeDelayInput.addEventListener('change',function(e){
+placeholderKeystrokeDelayInput.addEventListener('change', function (e) {
 	const delay = e.target.value;
 	settings.typewriterDelayCharacter = delay;
-	document.getElementById('main-searchbar').setAttribute('typewriterDelayCharacter', delay);
-	document.getElementById('main-floater').setAttribute('typewriterDelayCharacter', delay);
 	renderCode();
 })
 
-function addPlaceholder(text){
+function addPlaceholder(text) {
 	settings.placeholders.push(text);
-	document.getElementById('main-searchbar').setAttribute('placeholders', JSON.stringify(settings.placeholders));
-	document.getElementById('main-floater').setAttribute('placeholders', JSON.stringify(settings.placeholders));
 	renderPlaceholders();
 }
-function removePlaceholder(e){
+function removePlaceholder(e) {
 	const index = parseInt(e.target.getAttribute('index'))
-	console.log('removing placeholder',index);
-	settings.placeholders.splice(index,1);
-	document.getElementById('main-searchbar').setAttribute('placeholders', JSON.stringify(settings.placeholders));
-	document.getElementById('main-floater').setAttribute('placeholders', JSON.stringify(settings.placeholders));
+	console.log('removing placeholder', index);
+	settings.placeholders.splice(index, 1);
 	renderPlaceholders()
 }
 
-function renderPlaceholders(){
+function renderPlaceholders() {
 	let phtml = '';
-	for(let i=0; i<settings.placeholders.length;++i){
-		phtml+=`<li id="placeholder-${i}"><span class="placeholder-remove-icon" index="${i}" onclick="removePlaceholder(event)">❌</span>${settings.placeholders[i]}</li>`
+	for (let i = 0; i < settings.placeholders.length; ++i) {
+		phtml += `<li id="placeholder-${i}"><span class="placeholder-remove-icon" index="${i}" onclick="removePlaceholder(event)">❌</span>${settings.placeholders[i]}</li>`
 	}
 	placeholderList.innerHTML = phtml;
 	renderCode();
 }
 
 function renderCode() {
-	floaterContainer.innerHTML = floaterContainer.innerHTML;
-	searchbarContainer.innerHTML = searchbarContainer.innerHTML;
-	resultsContainer.innerHTML = resultsContainer.innerHTML;
-	console.log('rendering...')
-	const code = `
-	<body>
-${settings.showSearch ?
+	let searchbarCode = `
+	${settings.showSearch ?
 			`\n<jina-searchbar\
 	${settings.theme != defaultComponentSettings.theme ? `\ntheme="${settings.theme}"` : ''}\
 	${settings.resultsAreaId ? `\nresultsAreaId="${settings.resultsAreaId}"` : ''}\
 	${settings.typewriterEffect ? `\ntypewriterEffect="true"` : ''}\
-	${settings.typewriterDelayCharacter!=defaultComponentSettings.typewriterDelayCharacter ? `\ntypewriterDelayCharacter=${settings.typewriterDelayCharacter}` : ''}\
-	${settings.typewriterDelayItem!=defaultComponentSettings.typewriterDelayItem ? `\ntypewriterDelayItem=${settings.typewriterDelayItem}` : ''}\
-	${String(settings.placeholders)!=String(defaultComponentSettings.placeholders) ? `\nplaceholders='${JSON.stringify(settings.placeholders)}'` : ''}\
-	${settings.resultsLocation=='external' ?'\nresultsLocation="external"':''}\
-	${settings.showSearchbarDropzone ?'':'\nshowDropzone="false"'}>
-</jina-searchbar>\n`
-				: ''}\
-${settings.resultsLocation=='external' ?'\n<jina-results></jina-results>':''}
-${settings.showFloater ?
-			`\n<jina-floater\
+	${settings.typewriterDelayCharacter != defaultComponentSettings.typewriterDelayCharacter ? `\ntypewriterDelayCharacter=${settings.typewriterDelayCharacter}` : ''}\
+	${settings.typewriterDelayItem != defaultComponentSettings.typewriterDelayItem ? `\ntypewriterDelayItem=${settings.typewriterDelayItem}` : ''}\
+	${String(settings.placeholders) != String(defaultComponentSettings.placeholders) ? `\nplaceholders='${JSON.stringify(settings.placeholders)}'` : ''}\
+	${settings.resultsLocation == 'external' ? '\nresultsLocation="external"' : ''}\
+	${settings.showSearchbarDropzone ? '' : '\nshowDropzone="false"'}>
+	</jina-searchbar>`: ''}\
+	`;
+
+	let floaterCode = `
+	${settings.showFloater ?
+			`\n${settings.floaterStyle === 'standard' ? '<jina-floater' : '<jina-floater-chat'}\
 	${settings.theme != defaultComponentSettings.theme ? `\ntheme="${settings.theme}"` : ''}\
 	${settings.typewriterEffect ? `\ntypewriterEffect="true"` : ''}\
-	${settings.typewriterDelayCharacter!=defaultComponentSettings.typewriterDelayCharacter ? `\ntypewriterDelayCharacter=${settings.typewriterDelayCharacter}` : ''}\
-	${settings.typewriterDelayItem!=defaultComponentSettings.typewriterDelayItem ? `\ntypewriterDelayItem=${settings.typewriterDelayItem}` : ''}\
-	${String(settings.placeholders)!=String(defaultComponentSettings.placeholders) ? `\nplaceholders='${JSON.stringify(settings.placeholders)}'` : ''}\
-	${settings.showFloaterDropzone ?'':'\nshowDropzone="false"'}>
-	</jina-floater>\n`
-        : ''}
-<body>
+	${settings.typewriterDelayCharacter != defaultComponentSettings.typewriterDelayCharacter ? `\ntypewriterDelayCharacter=${settings.typewriterDelayCharacter}` : ''}\
+	${settings.typewriterDelayItem != defaultComponentSettings.typewriterDelayItem ? `\ntypewriterDelayItem=${settings.typewriterDelayItem}` : ''}\
+	${String(settings.placeholders) != String(defaultComponentSettings.placeholders) ? `\nplaceholders='${JSON.stringify(settings.placeholders)}'` : ''}\
+	${settings.showFloaterDropzone ? '' : '\nshowDropzone="false"'}>
+	${settings.floaterStyle === 'standard' ? '</jina-floater>' : '</jina-floater-chat>'}` : ''}
+	`;
+
+	let resultsCode = `
+	${settings.resultsLocation == 'external' ? '<jina-results></jina-results>' : ''}\
+	`
+
+	floaterContainer.innerHTML = floaterCode;
+	searchbarContainer.innerHTML = searchbarCode;
+	resultsContainer.innerHTML = resultsCode;
+
+	const code = `
+	<body>\
+		${searchbarCode}\
+		${resultsCode}\
+		${floaterCode}
+	<body>
 
 	<script src="https://unpkg.com/jinabox"></script>
 <script>
 JinaBox.init('${settings.url}'${(settings.timeout || settings.top_k) ? `,{\
-	${settings.timeout?`timeout:${settings.timeout},`:''}\
-	${settings.top_k?`top_k:${settings.top_k}`:''}}` : ''});
+	${settings.timeout ? `timeout:${settings.timeout},` : ''}\
+	${settings.top_k ? `top_k:${settings.top_k}` : ''}}` : ''});
 </script>
-`
-    document.getElementById('jinabox-code').innerText = code;
+`;
+
+	document.getElementById('jinabox-code').innerText = code;
 }
 
 renderCode();
@@ -138,29 +142,29 @@ renderCode();
 const inputEndpoint = document.getElementById('inputEndpoint');
 inputEndpoint.setAttribute('value', initialEndpoint || '');
 inputEndpoint.addEventListener('input', function (e) {
-    const url = e.target.value;
-    localStorage.setItem('jina-endpoint', url);
-    window.JinaBox.updateSettings({url});
-    settings.url = url;
-    renderCode();
+	const url = e.target.value;
+	localStorage.setItem('jina-endpoint', url);
+	window.JinaBox.updateSettings({ url });
+	settings.url = url;
+	renderCode();
 });
 
 const inputTimeout = document.getElementById('inputTimeout');
 inputTimeout.setAttribute('value', defaultInitSettings.timeout);
 inputTimeout.addEventListener('input', function (e) {
-    const timeout = e.target.value;
-    window.JinaBox.updateSettings({timeout});
-    settings.timeout = timeout;
-    renderCode()
+	const timeout = e.target.value;
+	window.JinaBox.updateSettings({ timeout });
+	settings.timeout = timeout;
+	renderCode()
 });
 
 const inputTopk = document.getElementById('inputTopk');
 inputTopk.setAttribute('value', defaultInitSettings.top_k);
 inputTopk.addEventListener('input', function (e) {
-    const top_k = e.target.value;
-    window.JinaBox.updateSettings({top_k});
-    settings.top_k = top_k;
-    renderCode()
+	const top_k = e.target.value;
+	window.JinaBox.updateSettings({ top_k });
+	settings.top_k = top_k;
+	renderCode()
 });
 
 document.getElementById('showSearch').addEventListener('change', function (e) {
@@ -190,24 +194,12 @@ document.getElementById('showFloater').addEventListener('change', function (e) {
 document.getElementById('showSearchbarDropzone').addEventListener('change', function (e) {
 	const checked = e.target.checked;
 	settings.showSearchbarDropzone = checked;
-	if(checked){
-		document.getElementById('main-searchbar').setAttribute('showDropzone', 'true');
-	}
-	else{
-		document.getElementById('main-searchbar').setAttribute('showDropzone', 'false');
-	}
 	renderCode()
 });
 
 document.getElementById('showFloaterDropzone').addEventListener('change', function (e) {
 	const checked = e.target.checked;
 	settings.showFloaterDropzone = checked;
-	if(checked){
-		document.getElementById('main-floater').setAttribute('showDropzone', 'true');
-	}
-	else{
-		document.getElementById('main-floater').setAttribute('showDropzone', 'false');
-	}
 	renderCode()
 });
 
@@ -217,36 +209,33 @@ for (let i = 0; i < themes.length; ++i) {
 	let radio = themes[i];
 	radio.onclick = function () {
 		settings.theme = radio.value;
-		document.getElementById('main-searchbar').setAttribute('theme', settings.theme);
-		document.getElementById('main-floater').setAttribute('theme', settings.theme);
 		renderCode();
 	}
 }
 
-document.querySelector('#resultsDropdown').onclick = function(){
-	settings.resultsLocation = 'dropdown';
-	document.getElementById('main-searchbar').setAttribute('resultsLocation', 'dropdown');
+document.querySelector('#floaterStandard').onclick = function () {
+	settings.floaterStyle = 'standard';
 	renderCode()
 }
 
-document.querySelector('#resultsComponent').onclick = function(){
+document.querySelector('#floaterChatBot').onclick = function () {
+	settings.floaterStyle = 'chatBot';
+	renderCode()
+}
+
+document.querySelector('#resultsDropdown').onclick = function () {
+	settings.resultsLocation = 'dropdown';
+	renderCode()
+}
+
+document.querySelector('#resultsComponent').onclick = function () {
 	settings.resultsLocation = 'external';
-	document.getElementById('main-searchbar').setAttribute('resultsLocation', 'external');
 	renderCode()
 }
 
 document.getElementById('useTypewriter').addEventListener('change', function (e) {
 	const checked = e.target.checked;
 	settings.typewriterEffect = checked;
-
-	if(checked){
-		document.getElementById('main-searchbar').setAttribute('typewriterEffect', checked);
-		document.getElementById('main-floater').setAttribute('typewriterEffect', checked);
-	}
-	else{
-		document.getElementById('main-searchbar').removeAttribute('typewriterEffect');
-		document.getElementById('main-floater').removeAttribute('typewriterEffect');
-	}
 	renderCode();
 });
 
